f982f0bb872d1cc52bd38bfadc3d206a4513744a
Yar <yarsher.dev@gmail.com>
2019-03-28 00:07:29 -0400

    retrofit

--- app/src/main/java/com/yar/retrofit/JsonPlaceHolderAPI.java
+++ app/src/main/java/com/yar/retrofit/JsonPlaceHolderAPI.java
@@ -1,4 +1,15 @@
 package com.yar.retrofit;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface JsonPlaceHolderAPI {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+
+
 }
