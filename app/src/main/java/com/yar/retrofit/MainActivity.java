5e74b4ba1ea32f404f1b76445cba1658d65245c4
Yar <yarsher.dev@gmail.com>
2019-03-27 23:27:51 -0400

    init

--- app/src/main/java/com/yar/retrofit/MainActivity.java
+++ app/src/main/java/com/yar/retrofit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yar.retrofit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void retrofitSimpleExample(View view) {
+        Intent intent = new Intent(this, RetrofitSimpleExample.class);
+        startActivity(intent);
 
     }
 }
